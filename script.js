6936d2e7ef6541c8699dc11c8fd6f1437e516ab2
Marius Aleksander <marraleks@hotmail.com>
2020-02-20 11:05:40 +0100

    ChatBoxOption

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ const bilde1 = document.querySelector(".bilde1");
 const bakgrunn1 = document.querySelector(".bakgrunn1");
 const door = document.querySelector(".door");
 const profiler = document.querySelector(".profiler");
-
-
+const chatContainer = document.querySelector(".chat-container");
+const yes = document.querySelector(".yes");
+const no = document.querySelector(".no");
 
 bilde1.onclick = () => {
-    bakgrunn1.classList.toggle("visible")
     profiler.style.display = "none"
+    bakgrunn1.classList.toggle("visible")
     
     door.onclick = () => {
-        bakgrunn1.classList.toggle("visible")
         profiler.style.display = "flex"
+        chatContainer.classList.toggle("visible")
+            yes.onclick = () => {
+                bakgrunn1.classList.toggle("visible")
+            }
+            no.onclick = () => {
+                chatContainer.classList.toggle("visible")
+            }
     }
 }
 
