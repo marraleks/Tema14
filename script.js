dc7063b0ad49db94e0be4a58ce561866bc715616
Marius Aleksander <marraleks@hotmail.com>
2020-02-20 10:55:00 +0100

    ChatBox

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const bilde1 = document.querySelector(".bilde1");
 const bakgrunn1 = document.querySelector(".bakgrunn1");
 const door = document.querySelector(".door");
+const profiler = document.querySelector(".profiler");
+
 
 
 bilde1.onclick = () => {
     bakgrunn1.classList.toggle("visible")
+    profiler.style.display = "none"
     
     door.onclick = () => {
         bakgrunn1.classList.toggle("visible")
+        profiler.style.display = "flex"
     }
 }
 
