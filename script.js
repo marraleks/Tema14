2dd4280eac1d39cdd51f0c7af542878d7a3009cc
havardhovde <haavardhovde@gmail.com>
2020-02-20 11:33:53 +0100

    pølse

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const bilde1 = document.querySelector(".bilde1");
+const bilde2 = document.querySelector(".bilde2");
+const bilde3 = document.querySelector(".bilde3");
+const bilde4 = document.querySelector(".bilde4");
+const bilde5 = document.querySelector(".bilde5");
+const topText = document.querySelector('.topText')
+const select = document.querySelector('.select')
 const bakgrunn1 = document.querySelector(".bakgrunn1");
 const door = document.querySelector(".door");
 const profiler = document.querySelector(".profiler");
-
 
 
 bilde1.onclick = () => {
@@ -12,9 +17,67 @@ bilde1.onclick = () => {
     door.onclick = () => {
         bakgrunn1.classList.toggle("visible")
         profiler.style.display = "flex"
+        bilde1.classList.add("unclickable")
+        select()
     }
 }
 
+bilde2.onclick = () => {
+    bakgrunn1.classList.toggle("visible")
+    profiler.style.display = "none"
+    
+    door.onclick = () => {
+        bakgrunn1.classList.toggle("visible")
+        profiler.style.display = "flex"
+        bilde2.classList.add("unclickable")
+        select()
+    }
+}
 
+bilde3.onclick = () => {
+    bakgrunn1.classList.toggle("visible")
+    profiler.style.display = "none"
+    
+    door.onclick = () => {
+        bakgrunn1.classList.toggle("visible")
+        profiler.style.display = "flex"
+        bilde3.classList.add("unclickable")
+        select()
+    }
+}
 
+bilde4.onclick = () => {
+    bakgrunn1.classList.toggle("visible")
+    profiler.style.display = "none"
+    
+    door.onclick = () => {
+        bakgrunn1.classList.toggle("visible")
+        profiler.style.display = "flex"
+        bilde4.classList.add("unclickable")
+        select()
+    }
+}
 
+bilde5.onclick = () => {
+    bakgrunn1.classList.toggle("visible")
+    profiler.style.display = "none"
+    
+    door.onclick = () => {
+        bakgrunn1.classList.toggle("visible")
+        profiler.style.display = "flex"
+        bilde5.classList.add("unclickable")
+        select()
+    }
+}
+
+function select() {
+    if (
+    bilde1.classList.contains('unclickable')
+    && bilde2.classList.contains('unclickable')
+    && bilde3.classList.contains('unclickable')
+    && bilde4.classList.contains('unclickable')
+    && bilde5.classList.contains('unclickable') ) {
+        // console.log("test")
+        topText.innerHTML='Velg'
+    }
+}
