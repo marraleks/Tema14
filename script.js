4be5f385c2282830df739fae6538c43392497524
havardhovde <haavardhovde@gmail.com>
2020-02-27 12:47:57 +0100

    did lots of stuff

--- script.js
+++ script.js
@@ -9,15 +9,21 @@ const topText = document.querySelector('.topText')
 const select = document.querySelector('.select')
 const bakgrunn1 = document.querySelector(".bakgrunn1");
 const bakgrunn3 = document.querySelector(".bakgrunn3");
+const bakgrunn5 = document.querySelector(".bakgrunn5");
 const door = document.querySelector(".door");
 const profiler = document.querySelector(".profiler");
 const ending1 = document.querySelector(".ending1");
 const ending3 = document.querySelector(".ending3");
+const ending5 = document.querySelector(".ending5");
 const close = document.querySelector(".close");
 
 const chatContainer = document.querySelector(".chat-container");
 const yes = document.querySelector(".yes");
 const no = document.querySelector(".no");
+
+const kommentar = document.querySelectorAll('.kommentar')
+const ting = document.querySelectorAll('.ting')
+
 
 topText.innerHTML = "Velg din date"
 
@@ -27,11 +33,17 @@ bilde1.onclick = () => {
     
     door.onclick = () => {
         bilde1.classList.add("unclickable")
-        chatContainer.style.display = "block"
-        profiler.style.display = "flex"
+        if(chatContainer.style.display != "block"){
+            closeComments()
+            chatContainer.style.display = "block"
+        }else{
+            closeComments()
+        }
         yes.onclick = () => {
                 bakgrunn1.classList.toggle("visible")
                 chatContainer.style.display = "none"
+                profiler.style.display = "flex"
+
             }
             no.onclick = () => {
                 chatContainer.style.display = "none"
@@ -77,11 +89,11 @@ bilde4.onclick = () => {
 }
 
 bilde5.onclick = () => {
-    bakgrunn1.classList.toggle("visible")
+    bakgrunn5.classList.toggle("visible")
     profiler.style.display = "none"
     
-    door.onclick = () => {
-        bakgrunn1.classList.toggle("visible")
+    bakgrunn5.onclick = () => {
+        bakgrunn5.classList.toggle("visible")
         profiler.style.display = "flex"
         bilde5.classList.add("unclickable")
         choose()
@@ -119,9 +131,34 @@ function velgDate() {
             profiler.style.display = 'none'
             close.style.display = "block"
         }
+        bilde5.onclick = () => {
+            ending5.style.display = "block"
+            bakgrunn5.style.display = "none"
+            profiler.style.display = 'none'
+            close.style.display = "block"
+        }
     }
 }
 
 close.onclick = () => {
     location.reload()
+}
+
+
+for(let i = 0; i < ting.length; i++) {
+    ting[i].onclick = () => {
+            if(kommentar[i].style.display != "block"){
+                closeComments()
+                kommentar[i].style.display = "block"
+            }else{
+                kommentar[i].style.display = "none"
+            }
+    }
+}
+
+const closeComments = () => {
+    chatContainer.style.display = "none"
+    for(let i = 0; i < ting.length; i++) {
+        kommentar[i].style.display = "none"
+    }
 }
